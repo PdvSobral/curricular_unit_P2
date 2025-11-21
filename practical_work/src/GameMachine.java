e4ca4e8de0fa23b8287531a70037600ea8356f00
PdvSobral <->
2025-11-21 00:07:17 +0000

    Turned the Class enum into public ENUMs directly.

--- practical_work/src/GameMachine.java
+++ practical_work/src/GameMachine.java
@@ -2,6 +2,6 @@ public class GameMachine {
 	// the machines in the arcade. NO SLOTS OR GAMBLING, only skill games
 	Controls control_scheme;
 	Game game_in_machine;
-	ENUMS.MACHINE_STATE availabilityStatus;
+	MACHINE_STATE availabilityStatus;
 	int available_tickets;
 }
