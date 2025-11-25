a64bf5424d33682a118d3a8943791ae5634e8228
PdvSobral <->
2025-11-25 11:27:44 +0000

    Continuing menu system.

--- practical_work/src/Main.java
+++ practical_work/src/Main.java
@@ -4,10 +4,17 @@ import java.util.List;
 
 @SuppressWarnings("UnnecessaryModifier")
 public class Main {
+	static void clearScreen() {
+		System.out.print("\033[2J\033[H");
+		return;
+	}
+
 	static void receivedCTRLD() {
 		System.out.print("\n[!!] CTRL+D (EOF) received!. Translating to SIGINT...");
 		System.exit(0);
 	}
+
+	static void pass(){ System.out.println("NOTHING HERE (yet...)"); }
 
 	static final int DEBUG = -2;
 	static final int NORMAL = 0;
@@ -27,11 +34,57 @@ public class Main {
 			// TODO: Add a save here for what is needed
 			System.out.println("[!!] Exiting...");
 		}));
-		int temp;
+		int __temp;
 		while (true){
-			temp = Menu.getInstance().menu(MAINMENU, "MAIN MENU", (char) 1, RUNNING_MODE);
-			if (temp == 0) break;
-			if (temp == DEBUG){
+			__temp = Menu.getInstance().menu(MAINMENU, "MAIN MENU", (char) 1, RUNNING_MODE);
+			if (__temp == 0) break;
+			if (__temp == DEBUG){
+				pass();
+			} else if (__temp == 1){
+				managementMenu();
+			} else if (__temp == 2){
+				checksMenu();
+			} else {
+				System.out.println("Option not yet implemented!");
+			}
+		}
+		System.exit(0);
+	}
+
+	private static void managementMenu() throws IOException{
+		int __temp;
+		while (true){
+			__temp = Menu.getInstance().menu(MANGEGMENU, "MANAGEMENT MENU", (char) 1, RUNNING_MODE);
+			if (__temp == 0) return;
+			if (__temp == DEBUG){
+				pass();
+			} else if (__temp == 2) {
+				gameManeg();
+			} else {
+				System.out.println("Option not yet implemented!");
+			}
+		}
+	}
+
+	private static void checksMenu() throws IOException{
+		int __temp;
+		while (true){
+			__temp = Menu.getInstance().menu(CHECKSMENU, "CHECKS MENU", (char) 1, RUNNING_MODE);
+			if (__temp == 0) return;
+			if (__temp == DEBUG){
+				System.out.println("NOTHING TO DEBUG (yet...)");
+			} else {
+				System.out.println("Option not yet implemented!");
+			}
+		}
+	}
+
+	private static void gameManeg() throws IOException{
+		int __temp;
+		while (true){
+			__temp = Menu.getInstance().menu(GAMEMANGEG, "GAME MANAGEMENT", (char) 1, RUNNING_MODE);
+			if (__temp == 0) return;
+			if (__temp == DEBUG){
 				Database.getInstance().setMainSaveDirectory("./practical_work/db");
 				Database.getInstance().setGamesSubdirectory("games");
 				Game teste = new Game(2001, "Space Odessey", new ArrayList<>(), "space", "no idea", "");
@@ -40,18 +93,9 @@ public class Main {
 				System.out.println("Loading...");
 				Game test = Database.getInstance().loadGame("2001_Space-Odessey.gm");
 				System.out.println(test);
-			} else if (temp == 2){
-				System.out.println("2");
-			} else if (temp == 3){
-				System.out.println("3");
 			} else {
-				System.out.print("Option not yet implemented!");
+				System.out.println("Option not yet implemented!");
 			}
 		}
-		System.out.println("Exiting...");
-	}
-
-	private void management_menu(){
-
 	}
 }
